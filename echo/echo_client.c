5bcc8be0b77d58aa9ea2fb66765db5dcbc861134
ed <daisuke_settai@keio.jp>
2018-06-26 15:19:49 +0900

    second

--- echo/echo_client.c
+++ echo/echo_client.c
@@ -16,39 +16,36 @@ exit(EXIT_FAILURE);
 
 int main(int argc, char **argv)
 {
-int sockfd, len;
-char buf[BUFSIZ];
-struct sockaddr_in serv;
-unsigned short port;
-if(argc != 3)
-{
-printf("usage: progname serv_ip serv_port\n");
-exit(EXIT_FAILURE);
-}
-if((sockfd = socket(PF_INET, SOCK_STREAM, 0)) < 0)
-err_func("socket");
-serv.sin_family = PF_INET;
-port = (unsigned short)strtol(argv[2] , NULL , 10);
-serv.sin_port = htons(port);
-inet_aton(argv[1], &(serv.sin_addr));
+  int sockfd, len;
+  char buf[BUFSIZ];
+  struct sockaddr_in serv;
+  unsigned short port;
+  if(argc != 3){
+    printf("usage: progname serv_ip serv_port\n");
+    exit(EXIT_FAILURE);
+  }
+  if((sockfd = socket(PF_INET, SOCK_STREAM, 0)) < 0)
+    err_func("socket");
+  serv.sin_family = PF_INET;
+  port = (unsigned short)strtol(argv[2] , NULL , 10);
+  serv.sin_port = htons(port);
+  inet_aton(argv[1], &(serv.sin_addr));
 
-if(connect(sockfd, (struct sockaddr *)&serv, sizeof(struct sockaddr_in)) < 0)
-err_func("connect");
+  if(connect(sockfd, (struct sockaddr *)&serv, sizeof(struct sockaddr_in)) < 0)
+    err_func("connect");
 
-do
-{
-printf("-> ");
-fgets(buf, BUFSIZ, stdin);
-len = strlen(buf);
-
-len = send(sockfd, buf, len, 0);
-len = recv(sockfd, buf, len, 0);
-buf[len] = '\0';
-printf("<- %s\n", buf);
-}while(strncasecmp(buf, "EXIT\r\n", 6) != 0 && strncasecmp(buf, "EXIT\n", 5) != 0);
-
-close(sockfd);
-return 0;
-
+  do {
+    printf("-> ");
+    fgets(buf, BUFSIZ, stdin);
+    len = strlen(buf);
+    buf[len] = '\0';
+    len = strlen(buf);
+    len = send(sockfd, buf, len + 1, 0);
+    len = recv(sockfd, buf, len, 0);
+    printf("<- %s", buf);
+    } while (strncasecmp(buf, "EXIT\r\n", 6) != 0 && strncasecmp(buf, "EXIT\n", 5) != 0);
+ 
+  close(sockfd);
+  return 0;
 }
 
